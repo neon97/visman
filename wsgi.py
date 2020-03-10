9823b2b7180aed2d18eb99f7f7d26eeb40a58726
mayakraj <visitormanagement2019@gmail.com>
2020-03-10 22:28:37 +0530

    corrected db

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,9 @@ from vis_app import create_app
 app = create_app()
 """Create and return app."""
 #app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
+#app.config['SECRET_KEY'] = 'secret_key'
 # load_blueprints(app)
 
 #bcrypt = Bcrypt(app)
-app.run(debug=True)
+#app.run(debug=True)
 #app.run()
